d8f7546489598fff138f70d7d111a1014b7afd2c
Ashton S Bradley <ashton.bradley@gmail.com>
2017-09-24 22:41:15 +1300

    regenerate runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,10 @@ using VortexDistributions
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+include("testallcharges.jl")
+include("testallpositions.jl")
+include("makepsi.jl")
+include("checkpositions.jl")
+tic()
+@testset "Vortex location and charge tests " begin include("vortextests.jl") end
+toc()
