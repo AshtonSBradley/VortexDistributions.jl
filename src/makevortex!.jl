a3af79984ef58c5a432d81c0b39116e73b054da0
Ashton Bradley <ashton.bradley@gmail.com>
2018-02-01 08:46:26 +1300

    makevortex! tests

--- src/makevortex!.jl
+++ src/makevortex!.jl
@@ -1,11 +1,12 @@
 function makevortex!(ψ,vortex,x,y,ξ)
     x0, y0, σ0 = vortex
-    ϕ = angle.(ψ)
-    U = abs.(ψ)
+    #ϕ = angle.(ψ)
+    #U = complex(abs.(ψ))
     X = x-x0
     Y = y-y0
     R = @. sqrt(X^2+Y^2)
-    ϕ .+= @. σ0*atan2(X,Y)
-    χ = @. U*vortexcore(R,ξ)
-    ψ . =  @. U*exp(im*ϕ)
+    #ϕ .+= σ0*atan2.(X,Y)
+    #χ = @. U*vortexcore(R,ξ)
+    #ψ .=  U.*exp.(im*ϕ)
+    ψ .= ψ.*vortexcore(R,ξ).*exp.(im*σ0*atan2.(X,Y))
 end
--- test/testmakevortex.jl
+++ test/testmakevortex.jl
@@ -30,13 +30,19 @@ testvort = sortrows(testvort)
 
 ξ = 0.1
 #construct vortices
-vortex = zeros(2,3)
-vortex[1,:] = [0.2*Lx,0*Ly,1]
-vortex[2,:] = [0.6*Lx,-0.4*Ly,-1]
-ψ = ones(x*y)
-sz=size(vortex)
-for j=1:sz[1]
+#
+Nv = 2
+vortex = zeros(Nv,3)
+vortex[2,:] = [0.2*Lx,0*Ly,1]
+vortex[1,:] = [0.6*Lx,-0.4*Ly,-1]
+ψ = complex(ones(x*y))
+
+for j=1:Nv
     makevortex!(ψ,vortex[j,:],x,y,ξ)
 end
+
 ϕ = angle.(ψ)
 pcolormesh(x*ones(y)/Lx,ones(x)*y/Ly,ϕ)
+colorbar()
+
+vortex
