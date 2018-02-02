60901eb11a1f313ef51768ff100d5a1ab3fa33b1
AshtonSBradley <ashton.bradley@otago.ac.nz>
2018-02-02 14:54:54 +1300

    makevortex updates

--- src/makevortex.jl
+++ src/makevortex.jl
@@ -1,4 +1,7 @@
 function makevortex(ψ,vortex,x,y,ξ)
+    @assert typeof(x)==Array{Float64,1}
+    @assert typeof(y)==Array{Float64,1}
+    @assert typeof(ψ)==Array{Complex{Float64},2}
     x0, y0, σ0 = vortex
     X = x-x0; Y = y'-y0
     R = @. sqrt(X^2+Y^2)
