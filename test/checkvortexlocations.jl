049b664d974e621aea1db3b86cbb6598bf942896
Ashton Bradley <ashton.bradley@gmail.com>
2018-07-16 19:37:43 -0700

    fix location tests -> now pass

--- test/checkvortexlocations.jl
+++ test/checkvortexlocations.jl
@@ -3,7 +3,7 @@ function checkvortexlocations(testvort,vortices,x,y,Nv)
 dx = x[2] - x[1]; dy = y[2] - y[1]
 vortfound = 0
 for j=1:Nv
-    if abs.(testvort[j,1]-vortices[j,1])<dx &&                   abs.(testvort[j,2]-vortices[j,2])<dy
+    if (isapprox(testvort[j,1],vortices[j,1],atol=dx) && isapprox(testvort[j,2],vortices[j,2],atol=dy))
         vortfound+=1
     end
 end
