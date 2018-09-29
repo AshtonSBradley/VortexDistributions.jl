a39d50a33f323766a3aef73bd0ab8e9c96c2f01d
Ashton Bradley <ashton.bradley@gmail.com>
2018-09-29 21:10:11 +1200

    reduce test size

--- test/vortextests.jl
+++ test/vortextests.jl
@@ -1,8 +1,8 @@
 
 @test testall_charges(1)
 @test testall_charges(3)
-@test testall_charges(30)
+@test testall_charges(10)
 
 @test testall_positions(1)
 @test testall_positions(3)
-@test testall_positions(30)
+@test testall_positions(10)
