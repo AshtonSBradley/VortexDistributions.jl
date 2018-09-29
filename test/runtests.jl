0200d8a1b475d67e19fd159e172c902064610ea4
Ashton Bradley <ashton.bradley@gmail.com>
2018-09-29 20:07:22 +1200

    tidy up

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,12 +7,3 @@ include("testall_positions.jl")
 include("makepsi.jl")
 include("checkvortexlocations.jl")
 @testset "Vortex location and circulation tests " begin include("vortextests.jl") end
-
-#=
-Nv = 5
-x,y,psi,testvort = makepsi(Nv)
-testvort
-nt,np,nn,vortices = findvortices(psi,x,y)
-vortices = remove_edgevortices(vortices,x,y)
-chargesfound = (vortices[:,3] == testvort[:,3])
-=#
