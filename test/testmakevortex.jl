6f667e1b57f93a011bc7b68ef2cabb41e77f2a2d
Ashton Bradley <ashton.bradley@gmail.com>
2018-09-29 21:49:37 +1200

    tidy

--- test/testmakevortex.jl
+++ test/testmakevortex.jl
@@ -1,15 +1,14 @@
 using VortexDistributions, PyPlot, LinearAlgebra, Revise
 
 # create some grids
-Nv = 10
 Lx = 300.; Ly = 150.
 Nx = 1000; Ny = 500
 x = linspace(-Lx/2,Lx/2,Nx)
 y = linspace(-Ly/2,Ly/2,Ny)
-
-testvort = randomvortices(x,y,Nv)
-
 ξ = 1.0
+
+Nv = 10
+testvort = randomvortices(x,y,Nv)
 
 ψ = ones(size(x.*y')) |> complex
 
@@ -17,7 +16,6 @@ makeallvortices!(ψ,testvort,x,y,ξ)
 
 psi = zeros(Ny,Nx) |> complex
 transpose!(psi,ψ)
-#psi = reverse(psi,dims=1)
 ϕ = angle.(psi)
 pcolormesh(x,y,ϕ)
 xlabel("x");ylabel("y")
