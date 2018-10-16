67e125e4f8e569690c1ff7e4452d24266d39223c
AshtonSBradley <ashton.bradley@otago.ac.nz>
2018-10-17 11:51:39 +1300

    tidy up

--- src/VortexDistributions.jl
+++ src/VortexDistributions.jl
@@ -6,7 +6,6 @@ using Reexport
 @reexport using Interpolations
 
 Interpolations.interpolate(A::Array{Complex{Float64},2})=interpolate(real(A))+im*interpolate(imag(A))
-
 include("findvortices.jl")
 include("unwrap.jl")
 include("makevortex.jl")
