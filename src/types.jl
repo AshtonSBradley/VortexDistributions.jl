aa1c2fc326590e48777685290fd7971a2f71a625
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2019-12-13 22:36:49 +1300

    more docs

--- src/types.jl
+++ src/types.jl
@@ -2,12 +2,32 @@ abstract type Field end
 abstract type Vortex end
 abstract type CoreShape end
 
+"""
+    Torus <: Field
+
+Specify toroidal boundary conditions: doubly periodic in 2D.
+
+# Arguments
+- `ψ::Array{Complex{Float64},2}`: wavefunction.
+- `x::Vector{Float64}`: `x` coordinates.
+- `y::Vector{Float64}`: `y` coordinates.
+"""
 @with_kw mutable struct Torus <: Field
     ψ::Array{Complex{Float64},2}
     x::Vector{Float64}
     y::Vector{Float64}
 end
 
+"""
+    Sphere <: Field
+
+Specify spherical boundary conditions in 2D.
+
+# Arguments
+- `ψ::Array{Complex{Float64},2}`: wavefunction.
+- `x::Vector{Float64}`: `x` coordinates.
+- `y::Vector{Float64}`: `y` coordinates.
+"""
 @with_kw mutable struct Sphere <: Field
     ψ::Array{Complex{Float64},2}
     x::Vector{Float64}
