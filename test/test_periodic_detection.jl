3eb4629f6b1484de5bf27bacfa76555d7deb0402
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-05-06 08:42:53 +1200

    small test changes

--- test/test_periodic_detection.jl
+++ test/test_periodic_detection.jl
@@ -1,24 +1,22 @@
 using Pkg;Pkg.activate(".")
 using Test, Plots, Revise, VortexDistributions
 
-## readme test
-# make a periodic dipole at boundary
+## field
 Nx = 400; Ny = Nx
 Lx = 200; Ly = Lx
 x = LinRange(-Lx/2,Lx/2, Nx+1)[1:end-1]; y = x
 psi0 = one.(x*y') |> complex
 
-dx = x[2]-x[1]
-# doubly periodic boundary conditions
-psi = Torus(psi0,x,y)
+## make dipole
+# Periodic boundary conditions
+psi = Torus(copy(psi0),x,y)
 
-# test dipole
-xp = 1.1
+xp = 1.7
 yp = 2.1
 vp = PointVortex(xp,yp,1)
 
-xn = 2.1
-yn = 3.1
+xn = pi
+yn = 6.
 vn = PointVortex(xn,yn,-1)
 
 dip = ScalarVortex([vp;vn])
@@ -27,4 +25,5 @@ periodic_dipole!(psi,dip)
 
 heatmap(x,y,angle.(psi.ψ))
 
+## detect
 vort = findvortices(psi)
