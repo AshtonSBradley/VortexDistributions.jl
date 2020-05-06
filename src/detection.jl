348e4bfb17fe588c8364a1fdcfa08cdd66b82629
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-05-06 15:57:46 +1200

    refactoring for periodic...

--- src/detection.jl
+++ src/detection.jl
@@ -44,12 +44,16 @@ end
 
 function findvortices_interp(psi::Field)
     vort = findvortices_grid(psi,shift=true)
-    vort = removeedgevortices(vort,psi)
+    vort = removeedgevortices(vort,psi) # essential for current foundNear!
     #TODO: allow for interp with periodic data (here edges are stripped)
+    #NOTE: foundNear uses randVortexField which requires removing endgevortices
+    #to make periodic we have to remove all statements that remove edge vortices,
+    #except inside ingerpolation which will generate spurious edge vortices.
 
     for (j,vortex) in enumerate(vort)
         try
         vortz,psiz = corezoom(vortex,psi)
+        # vortz,psiz = corezoom_periodic(vortex,psi)
         vortz,psiz = corezoom(vortz,psiz)
         vortz,psiz = corezoom(vortz,psiz)
         vort[j] = vortz
@@ -63,24 +67,34 @@ function findvortices_jumps(psi::Field;shift=true)
     @unpack x,y,ψ = psi
     phase = angle.(ψ)
 
-    # Compute all nearest neighbour jumps
+    # Nearest neighbour phase jumps
     diffx = phasejumps(phase,1); diffy = phasejumps(phase,2)
 
-    # Compute all plaquette loop integrals with in-place memory recycling
+    # Plaquette loop integrals with in-place memory recycling
     circshift!(phase,diffx,(0,1))
     diffx .-= phase; diffx .-= diffy
     circshift!(phase,diffy,(1,0))
     diffx .+= phase
 
-    # Find all windings
+    # Windings
     ixp,iyp,vp = findwhere(diffx .> 0.0)
     xp = x[ixp]; yp = y[iyp]; np = length(vp)
     ixn,iyn,vn = findwhere(diffx .< 0.0)
     xn = x[ixn]; yn = y[iyn]; nn = length(vn)
 
     if shift
-        dx = x[2]-x[1]; dy = y[2] - y[1]
+        dx,dy=Δ(x),Δ(y)
         xp .-= dx/2; yp .-= dy/2; xn .-= dx/2; yn .-= dy/2
+        Lx = x[end]-x[1]+dx
+        Ly = y[end]-y[1]+dy
+        @. xp[xp>Lx/2] -= Lx
+        @. xp[xp<-Lx/2] += Lx
+        @. xn[xn>Lx/2] -= Lx
+        @. xn[xn<-Lx/2] += Lx
+        @. yp[yp>Ly/2] -= Ly
+        @. yp[yp<-Ly/2] += Ly
+        @. yn[yn>Ly/2] -= Ly
+        @. yn[yn<-Ly/2] += Ly
     end
 
     vortices = [xn yn -vn; xp yp vp]
@@ -97,9 +111,9 @@ Uses local interpolation to resolve core location.
 function corezoom(vortex::PointVortex,psi::T,winhalf=2,Nz=30) where T<:Field
     @unpack ψ,x,y = psi
     xv,yv,qv = rawData(vortex)
-    dx=x[2]-x[1]; dy=y[2]-y[1]
-    ixv = isapprox.(x,xv,atol=dx) |> findlast
-    iyv = isapprox.(y,yv,atol=dy) |> findlast
+    dx,dy=Δ(x),Δ(y)
+    ixv = isapprox.(x,xv,atol=dx) |> findfirst
+    iyv = isapprox.(y,yv,atol=dy) |> findfirst
     ixwin = (ixv-winhalf):(ixv+winhalf-1)
     iywin = (iyv-winhalf):(iyv+winhalf-1)
     xw = x[ixwin]; yw = y[iywin]; psiw = ψ[ixwin,iywin]
@@ -115,3 +129,27 @@ function corezoom(vortex::PointVortex,psi::T,winhalf=2,Nz=30) where T<:Field
 end
 
 corezoom(vortex::Array{PointVortex,1},psi::Field,winhalf=2,Nz=30) = corezoom(vortex[1],psi,2,30)
+
+function corezoom_periodic(vortex::PointVortex,psi::T,winhalf=2,Nz=30) where T<:Field
+    @unpack ψ,x,y = psi
+    xv,yv,qv = rawData(vortex)
+    dx,dy = Δ(x),Δ(y)
+    ixv = isapprox.(x,xv,atol=dx) |> findfirst
+    iyv = isapprox.(y,yv,atol=dy) |> findfirst
+    ixwin = (ixv-winhalf):(ixv+winhalf-1)
+    iywin = (iyv-winhalf):(iyv+winhalf-1)
+    xw = (x[ixv]-winhalf*dx):(x[ixv]+winhalf*dx-dx)
+    yw = (y[iyv]-winhalf*dy):(y[iyv]+winhalf*dy-dy)
+    psiw = ψ[mod1.(ixwin,end),mod1.(iywin,end)]
+    knots = (xw,yw)
+    itp = interpolate(knots, psiw, Gridded(Linear()))
+    xz = LinRange(xw[1],xw[end],Nz)
+    yz = LinRange(yw[1],yw[end],Nz)
+    psiz = itp(xz,yz)
+    ψv = T(psiz,xz |> Vector,yz |> Vector)
+    vortz = findvortices_grid(ψv,shift=false)
+    vortz = removeedgevortices(vortz,ψv)[1]
+    return vortz,ψv
+end
+
+corezoom_periodic(vortex::Array{PointVortex,1},psi::Field,winhalf=2,Nz=30) = corezoom_periodic(vortex[1],psi,2,30)
