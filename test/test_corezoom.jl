7cd0de0dc6eeaac56bee1863dc6016353d40502c
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-06-05 12:29:41 +1200

    corezoom updates

--- test/test_corezoom.jl
+++ test/test_corezoom.jl
@@ -2,33 +2,6 @@ using Pkg;Pkg.activate(".")
 using Test, Plots, Revise, VortexDistributions
 gr(transpose=true)
 
-## definition
-using ParameterizedFunctions, Interpolations
-"""
-    vortz,psiz = corezoom(vortex,ψ<:Field,winhalf=2,Nz=30)
-
-Uses local interpolation to resolve core location.
-"""
-function corezoom(vortex::PointVortex,psi::T,winhalf=2,Nz=30) where T<:Field
-    @info "made it here..."
-    @unpack ψ,x,y = psi
-    xv,yv,qv = vortex.x,vortex.y.vortex.q
-    dx,dy = Δ(x),Δ(y)
-    ixv = isapprox.(x,xv,atol=dx) |> findfirst
-    iyv = isapprox.(y,yv,atol=dy) |> findfirst
-    ixwin = (ixv-winhalf):(ixv+winhalf-1)
-    iywin = (iyv-winhalf):(iyv+winhalf-1)
-    xw = x[ixwin]; yw = y[iywin]; psiw = ψ[ixwin,iywin]
-    knots = (xw,yw)
-    itp = interpolate(knots, psiw, Gridded(Linear()))
-    xz = LinRange(xw[1],xw[end],Nz)
-    yz = LinRange(yw[1],yw[end],Nz)
-    psiz = itp(xz,yz)
-    ψv = T(psiz,xz |> Vector,yz |> Vector)
-    vortz = find_vortices_grid(ψv,shift=true)
-    vortz = remove_edge_vortices(vortz,ψv)[1]
-    return vortz,ψv
-end
 
 ## field with 2 points per healing length
 Nx = 400; Ny = 400
@@ -49,15 +22,49 @@ yn = 10.
 vn = PointVortex(xn,yn,-1)
 
 dip = ScalarVortex([vp;vn])
-# dip = Dipole(vp,vn)
-# vortex_array(dip.vp)[1:2]
-
 periodic_dipole!(psi,dip)
-
 heatmap(x,y,angle.(psi.ψ))
 
-## detect:  this must give sub-grid detection
+## definition
+using Parameters, Interpolations
+"""
+    vortz,psiz = corezoom(vortex,ψ<:Field,winhalf=2,Nz=30)
 
+Uses local interpolation to resolve core location.
+"""
+function corezoom(vortex::PointVortex,psi::T,winhalf=2,Nz=30) where T<:Field
+    # @info "made it here..."
+    @unpack ψ,x,y = psi
+    xv,yv,qv = vortex.x,vortex.y.vortex.q
+    dx,dy = Δ(x),Δ(y)
+    ixv = isapprox.(x,xv,atol=dx) |> findfirst
+    iyv = isapprox.(y,yv,atol=dy) |> findfirst
+    ixwin = (ixv-winhalf):(ixv+winhalf-1)
+    iywin = (iyv-winhalf):(iyv+winhalf-1)
+    xw = x[ixwin]; yw = y[iywin]; psiw = ψ[ixwin,iywin]
+    knots = (xw,yw)
+    itp = interpolate(knots, psiw, Gridded(Linear()))
+    xz = LinRange(xw[1],xw[end],Nz)
+    yz = LinRange(yw[1],yw[end],Nz)
+    psiz = itp(xz,yz)
+    ψv = T(psiz,xz |> Vector,yz |> Vector)
+    vortz = find_vortices_grid(ψv,shift=true)
+    vortz = remove_edge_vortices(vortz,ψv)[1]
+    return vortz,ψv
+end
+
+## zoom
+function zoom(psi::T,xc,yc,win=2) where T<: Field
+    @unpack ψ,x,y = psi
+    dx,dy = Δ(x),Δ(y)
+    ix = isapprox.(x,xc,atol=dx) |> findfirst
+    iy = isapprox.(y,yc,atol=dy) |> findfirst
+    ixw = (ix-win):(ix+win)
+    iyw = (iy-win):(iy+win)
+    xw = x[ixw]; yw = y[iyw]; psiw = ψ[ixw,iyw]
+return
+
+## detect:  this must give sub-grid detection
 vort = findvortices(psi)
 @show vort[1]
 
