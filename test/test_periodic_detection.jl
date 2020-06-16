2e3818c0d4e031b4d16d736d18f83975d94f0863
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-06-16 19:45:48 +1200

    vscode quiet

--- test/test_periodic_detection.jl
+++ test/test_periodic_detection.jl
@@ -7,7 +7,7 @@ gr(transpose=true)
 Nx = 400; Ny = 400
 Lx = 200; Ly = Lx
 x = LinRange(-Lx/2,Lx/2, Nx+1)[1:end-1]; y = LinRange(-Ly/2,Ly/2, Ny+1)[1:end-1];
-psi0 = one.(x*y') |> complex
+psi0 = one.(x*y') |> complex;
 
 ## make a field with a dipole
 # Periodic boundary conditions
