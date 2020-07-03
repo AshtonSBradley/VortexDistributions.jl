c0fc9068c883ba07b5765cdb2465262d337daeec
ashton.bradley@otago.ac.nz <ashton.bradley@gmail.com>
2020-07-04 09:07:49 +1200

    fix module defs

--- src/VortexDistributions.jl
+++ src/VortexDistributions.jl
@@ -15,21 +15,26 @@ using LightGraphs
 using SimpleWeightedGraphs
 
 const Λ = 0.8249
-export Field, Torus, Sphere
-export Vortex, CoreShape, Ansatz, Exact, ScalarVortex, PointVortex
-export scalar_ansatz, vortex_array, uniform
-export vortex!, findvortices, dipole_phase, periodic_dipole!
-export rand_charge, rand_pointvortex, rand_scalarvortex, rand_vortex, rand_vortexfield
-export found_near, phase_jumps, phase_jumps!, unwrap, unwrap!, Δ
-export Dipole, Cluster, VortexGroup, charge, xpos, ypos, pos
 
-export find_where, findvortices_jumps, findvortices_grid
-export remove_vortices_edge, zoom_interp, zoom_grid
-# export gpecore_exact, chebdif, getChebDMatrix, getChebD2Matrix
-# export ψi, ψa
+export Field, Torus, Sphere,
+Vortex, CoreShape, Ansatz, Exact, ScalarVortex, PointVortex,
+scalar_ansatz, vortex_array, uniform,
+vortex!, findvortices, dipole_phase, periodic_dipole!,
+rand_charge, rand_pointvortex, rand_scalarvortex, rand_vortex, rand_vortexfield,
+found_near, phase_jumps, phase_jumps!, unwrap, unwrap!, Δ,
+Dipole, Cluster, VortexGroup, charge, xpos, ypos, pos,
+
+find_where, findvortices_jumps, findvortices_grid,
+remove_vortices_edge, zoom_interp, zoom_grid,
+
+# RCA 
+get_dipoles, seed_clusters, distances, periodic_distances,
+sparse_distances, spanning_tree, vortex_marker, plot_vortices!,
+plot_cluster!, vortex_marker
 
 using .RecursiveClusterAlgorithm
 
+include("RecursiveClusterAlgothm/RecursiveClusterAlgorithm.jl")
 
 include("types.jl")
 include("pointvortex.jl")
