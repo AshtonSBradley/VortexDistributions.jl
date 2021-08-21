b94635c201c5568b07f280b34132452556f78a0e
timcop <timothy.copland98@gmail.com>
2021-08-21 16:23:44 +1200

    Some basic creation tests

---
+++ test/creations_test.jl
@@ -0,0 +1,7 @@
+using VortexDistributions, Test
+
+@testset "Creation Functions" begin 
+    @test scalar_ansatz(0) == 0
+    @test scalar_ansatz(1) == scalar_ansatz(-1)
+    @test typeof(Ansatz()) <: Ansatz 
+end 
