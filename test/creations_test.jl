7df59c3fc1eda47caf1f27d7aac5f3619c7222f6
timcop <timothy.copland98@gmail.com>
2021-08-21 19:24:56 +1200

    more simple tests

--- test/creations_test.jl
+++ test/creations_test.jl
@@ -3,5 +3,8 @@ using VortexDistributions, Test
 # @testset "Creation Functions" begin 
 @test scalar_ansatz(0) == 0
 @test scalar_ansatz(1) == scalar_ansatz(-1)
-@test typeof(Ansatz()) <: Ansatz 
-# end 
+f = Ansatz()
+x = LinRange(0, 10, 100)
+y = f.(x)
+@test length(y) == length(x)
+@test f.(3,4) == f(5)
---
+++ test_shit.jl
@@ -0,0 +1,9 @@
+using VortexDistributions, Parameters, LinearAlgebra
+
+foo = Ansatz()
+@unpack f, ξ, Λ = foo
+x = LinRange(0, 10, 100)
+y = f.(x)
+
+foo.(3,4)
+foo(5)
