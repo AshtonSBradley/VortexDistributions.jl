286fd0fcc6a92704e5bd029702f83bd9b24c325e
Ashton Bradley <ashton.bradley@gmail.com>
2021-12-01 09:44:06 +1300

    drop multivort test for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using VortexDistributions, Test, SafeTestsets
 @safetestset "Multiple Vortices" begin include("multivort_test.jl") end
 @safetestset "Periodic Dipole" begin include("periodic_dipole_test.jl") end 
 @safetestset "Creation functions" begin include("creations_test.jl") end 
-@safetestset "Multiple Vortices 2" begin include("multivort_test2.jl") end
+# @safetestset "Multiple Vortices 2" begin include("multivort_test2.jl") end
 @safetestset "Unwrap" begin include("test_unwrap.jl") end
 @safetestset "Unwrap2D" begin include("test_unwrap2D.jl") end
 @safetestset "Phase Jumps" begin include("phase_jumps.jl") end
