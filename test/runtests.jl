9c92bf16a88633a154bc3dedaf56216124f94a10
Ashton Bradley <ashton.bradley@gmail.com>
2021-12-01 16:36:19 +1300

    remove duplicate tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,6 @@ using VortexDistributions, Test, SafeTestsets
 @safetestset "Unwrap" begin include("test_unwrap.jl") end
 @safetestset "Unwrap2D" begin include("test_unwrap2D.jl") end
 @safetestset "Phase Jumps" begin include("phase_jumps.jl") end
-@safetestset "Periodic Dipole" begin include("periodic_dipole_test.jl") end
 @safetestset "Scalar Vortex Arguments" begin include("vortex_types.jl") end
 @safetestset "Single Vortex Sphere Field" begin include("single_vort_sphere.jl") end
 @safetestset "Keep Vortices" begin include("keep_vortices_test.jl") end
