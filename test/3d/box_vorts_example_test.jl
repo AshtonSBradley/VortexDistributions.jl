ab816999d63ebc77995e7c55c2a049ddc32b4ce8
Tim Copland <timothy.copland98@gmail.com>
2022-09-19 10:09:04 +1200

    added helper func + fix thread bug

--- test/3d/box_vorts_example_test.jl
+++ test/3d/box_vorts_example_test.jl
@@ -1,11 +1,11 @@
-using VortexDistributions, JLD2
+using VortexDistributions, JLD2, Test
 
 @load joinpath(@__DIR__, "box_vorts.jld2") psi_tubes1 X;
 
 psi = psi_tubes1;
 
-for N = 1:16
-
+for N = 13:16
+    print(N)
     vorts_3d = find_vortex_points_3d(psi, X, N);
 
     vorts_class = connect_vortex_points_3d(vorts_3d, X, 0., N, true)
@@ -17,3 +17,20 @@ for N = 1:16
     @test length(v_sort) == 3
 
 end
+
+# N = 4
+
+# vorts_3d = find_vortex_points_3d(psi, X, N)
+
+# fetch(vorts_3d)
+
+# vorts_3d
+
+# vorts_3d
+# vorts_class = connect_vortex_points_3d(vorts_3d, X, 0., N, true)
+
+# v_sort = sort_classified_vorts_3d(vorts_class, vorts_3d, X)
+
+# @test length(vorts_class) == 3
+
+# @test length(v_sort) == 3
